97cedba8c5b7ec35856f3eebef0141d9afa663f2
Forrest Parker <forresta.parker@gmail.com>
2014-09-17 10:17:03 -0700

    added test

--- deck_o_cards.rb
+++ deck_o_cards.rb
@@ -1,16 +1,17 @@
 
 
-  def create_deck
-    deck = []
-    values = %w[ Ace 2 3 4 5 6 7 8 9 10 Jack Queen King ]
-    suits = %w[ Hearts Diamonds Clubs Spades ]
-    suits.each do |suit|
-      values.each do |value|
-        deck << "#{value} of #{suit}"
-        puts deck
-      end
+def create_deck
+  deck = []
+  values = %w[Ace 2 3 4 5 6 7 8 9 10 Jack Queen King]
+  suits = %w[Hearts Diamonds Clubs Spades]
+  suits.each do |suit|
+    values.each do |value|
+      deck << "#{value} of #{suit}"
     end
-    puts deck.length
   end
+  puts deck.length == 52
+  puts deck[0] == "Ace of Hearts"
+  puts deck[13] == "Ace of Diamonds"
+end
 
 create_deck
